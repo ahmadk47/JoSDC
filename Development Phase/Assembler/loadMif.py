b5959c9496f33cfd5da239d293551e9570d3ccea
Saleh Ghobari <96133643+SalehGhobari@users.noreply.github.com>
2024-11-14 22:39:25 +0300

    Update loadMif.py

--- Development Phase/Assembler/loadMif.py
+++ Development Phase/Assembler/loadMif.py
@@ -1,14 +1,14 @@
 import subprocess
 
-# Paths
+
 assembler_directory = r"path to assembler"
 java_file_name = "Assembler"
 output_mif_file = r"path to .mif file"
 
-# Compile the Java Assembler
+
 subprocess.run(["javac", f"{assembler_directory}\\{java_file_name}.java"], cwd=assembler_directory, check=True)
 
-# Run the Assembler and redirect the output to the .mif file
+
 with open(output_mif_file, "w") as mif_file:
     subprocess.run(["java", java_file_name], cwd=assembler_directory, stdout=mif_file, text=True, check=True)
 
